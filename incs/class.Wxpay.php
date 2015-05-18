2b22577c43fccd79db6a0cc338ec0f4f51a78a16
GavinLai <laigw.vip@gmail.com>
2015-05-19 03:35:25 +0800

    添加微信支付SDK库

---
+++ incs/class.Wxpay.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * 微信支付接口入口类
+ *
+ * @author Gavin<laigw.vip@gmail.com>
+ */
+defined('IN_SIMPHP') or die('Access Denied');
+
+//定义微信支付SDK库根目录
+define('WXPAY_SDK_ROOT', __DIR__.'/libs/wxpay/');
+
+require_once WXPAY_SDK_ROOT."lib/WxPay.Api.php";
+
+class Wxpay {
+  
+  
+  
+}
+
+/*----- END FILE: class.Wxpay.php -----*/
