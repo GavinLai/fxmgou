ecaf432ddfa206b32af65e107fa0f4aa15a777b1
GavinLai <laigw.vip@gmail.com>
2015-05-22 03:17:07 +0800

    删除不在需要的modules

--- mobiles/node/tpl/mod_node_myshare.htm.php
+++
@@ -1,3 +0,0 @@
-<?php defined('IN_SIMPHP') or die('Access Denied');?>
-<article class="nodetxt" contenteditable="true" id="nodetxt">我刚刚购买了<a href="http://www.zhufuyu.cn/#/mall/detail/<?=$id?>">“<?=$content?>”</a><article>
-<?php include T($tpl_footer);?>
